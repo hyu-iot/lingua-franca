1cae37e5ce71b527c567863731f436010efe082c
Shaokai Lin <shaokai_lin@126.com>
2022-04-21 14:59:41 -0700

    Modify target property, c core file utils, and the ActionDelay test case.

--- org.lflang/src/org/lflang/generator/c/CCoreFilesUtils.java
+++ org.lflang/src/org/lflang/generator/c/CCoreFilesUtils.java
@@ -120,7 +120,9 @@ public class CCoreFilesUtils {
                     "threaded/scheduler.h",
                     "threaded/scheduler_instance.h",
                     "threaded/scheduler_sync_tag_advance.c",
-                    "threaded/reactor_threaded.c"
+                    "threaded/reactor_threaded.c",
+                    scheduler == SchedulerOption.QS ? "threaded/instruction_QS.h" : "",
+                    scheduler == SchedulerOption.QS ? "threaded/static_schedule.h" : ""
                 ),
                 scheduler.getRelativePaths().stream().map(path -> "threaded/" + path.toString().replace("\\", "/"))
             ).collect(Collectors.toList()) :
